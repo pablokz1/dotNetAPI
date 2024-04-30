39f473e3fb7352711f2000421037a7aa1d6f60d2
Pablo <Pablo@PABO>
2024-04-29 21:34:37 -0300

    Adicionar arquivos de projeto.

---
+++ dotNetAPI/Controllers/PessoasController.cs
@@ -0,0 +1,58 @@
+﻿using dotNetAPI.Models;
+
+namespace dotNetAPI.Controllers
+{
+    public static class PessoasController
+    {
+        public static List<Pessoas> Pessoas = new()
+        {
+            new(Guid.NewGuid(), "Neymar"),
+            new(Guid.NewGuid(), "CR7"),
+            new(Guid.NewGuid(), "Messi"),
+        };
+
+        public static void MapPessoasRotas(this WebApplication app)
+        {
+            app.MapGet("/pessoas", () => Pessoas);
+
+            app.MapGet("/pessoas/{name}",
+                (string name) => Pessoas.Find(x => x.Name == name));
+
+            app.MapPost("/pessoas",
+                (Pessoas pessoa) =>
+                {
+                    Pessoas.Add(pessoa);
+                    return pessoa;
+                });
+
+            app.MapPut("/pessoas/{id:guid}", (Guid id, Pessoas pessoa) =>
+                {
+                    var encontrado = Pessoas.Find(x => x.Id == id);
+                    if (encontrado == null)
+                    {
+                        Results.NotFound();
+                    }
+                    else
+                    {
+                        encontrado.Name = pessoa.Name;
+                    }
+
+                    return Results.Ok(encontrado);
+                });
+
+            app.MapDelete("/pessoas/{id}", (Guid id) =>
+            {
+                var encontrado = Pessoas.Find(x => x.Id == id);
+                if (encontrado == null)
+                {
+                    return Results.NotFound("Pessoa não encontrada.");
+                }
+                else
+                {
+                    Pessoas.Remove(encontrado);
+                    return Results.Ok("Pessoa removida com sucesso.");
+                }
+            });
+        }
+    }
+}
---
+++ dotNetAPI/Models/Pessoas.cs
@@ -0,0 +1,14 @@
+﻿namespace dotNetAPI.Models
+{
+    public class Pessoas
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Pessoas(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
