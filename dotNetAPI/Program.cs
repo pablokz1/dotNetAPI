41f0ed90d5566495780966d68d7b8b6c3ead3c0f
pablokz1 <pablo.carvalho16@hotmail.com>
2024-04-30 22:21:42 -0300

    Configurando CORS

--- dotNetAPI/Program.cs
+++ dotNetAPI/Program.cs
@@ -8,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => 
+options.AddDefaultPolicy(policy =>
+{
+    policy.AllowAnyOrigin();
+    policy.AllowAnyHeader();
+    policy.AllowAnyMethod();
+}));
 
 var app = builder.Build();
 
@@ -18,11 +25,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors();
 
 app.MapPessoasRotas();
 
